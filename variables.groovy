aba702ae74673bc6f8c4607012a01d1747ab1e0d
userariadna <66219761+userariadna@users.noreply.github.com>
2024-04-24 17:37:13 -0600

    Update variables.groovy

--- variables.groovy
+++ variables.groovy
@@ -1,1 +1,16 @@
+//Read headers
+def map = message.getHeaders();
+def temp = map.get("OrderNo")M
+
+
+//Read Body
+def bodyRead = message.getBody();
+
+
+//Read properties 
+def pro = message.getProperties();
+def propTemp = pro.get("Check");
+
+def finalStr = 'Concatenated_Value' + temp + propTemp; 
+
 
